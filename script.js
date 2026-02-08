10015320f7a6977d5082e657fc4926fe8fd09d4a
david <davidaiyede27@gmail.com>
2026-02-08 08:21:14 -0800

    Fix whatsapp flow

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const rejection = document.getElementById("rejection");
 const hi = document.querySelector(".hi")
 const audio = document.getElementById("loveSound")
 const audios = document.getElementById("hateSound")
+const whatsappBtn = document.getElementById("whatsappBtn")
+const whatsappBtns = document.getElementById("whatsappBtns")
+
 function yesClick(){
      audio.play ();
         buttons.classList.add("hide");
@@ -12,14 +15,11 @@ function yesClick(){
         celebration.style.display = "flex" ;
         hi.style.display= "none"
        }, 400);
-
-       setTimeout(() => {
-    const phoneNumber = "2348035525774";
-    const message = encodeURIComponent(
-        "Hey Praise I just clicked YES on your Valentine page "
-    );
-    window.location.href = "https://wa.me/" + phoneNumber + "?text=" + message;
-}, 7000);
+      
+      const phoneNumber = "2348035525774";
+      const message = encodeURIComponent("Hey Praise I just clicked YES on your Valentine page ");
+       whatsappBtn.href =  "https://wa.me/" + phoneNumber + "?text=" + message;
+       whatsappBtn.style.display = "inline-block";
 }
 function noClick(){
       audios.play ();
@@ -29,11 +29,8 @@ function noClick(){
         rejection.style.display = "flex" ;
         hi.style.display= "none"
        }, 400);
-          setTimeout(() => {
-    const phoneNumber = "2348035525774";
-    const message = encodeURIComponent(
-        "Hey Praise I just clicked NO on your Valentine page "
-    );
-    window.location.href = "https://wa.me/" + phoneNumber + "?text=" + message;
-}, 7000);
+         const phoneNumber = "2348035525774";
+      const message = encodeURIComponent("Hey Praise I just clicked NO on your Valentine page ");
+       whatsappBtns.href =  "https://wa.me/" + phoneNumber + "?text=" + message;
+       whatsappBtns.style.display = "inline-block";
 }
